e72ad69f72d7746bb9a4fe66d5fc2816a91ce09b
ekistrimova <90572032+ekistrimova@users.noreply.github.com>
2024-03-10 22:42:41 +0300

    Update tasks.h

--- include/tasks.h
+++ include/tasks.h
@@ -1,6 +1,6 @@
 // Copyright 2024 Ekaterina Kistrimova
-#ifndef ST_2_TASKS_H
-#define ST_2_TASKS_H
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
 
 double solvePlanetAndRopeTask(double planetRadius, double radiusIncrement);
 
@@ -9,6 +9,6 @@ double solveSwimmingPoolTask(double poolRadius,
                              double costOfCoverage,
                              double fencePrice);
 
-#endif //ST_2_TASKS_H
+#endif //INCLUDE_TASKS_H_
 
 
