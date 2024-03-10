9f158d894cbe8d577456dabaf8bad6da0cd712b0
ekistrimova <90572032+ekistrimova@users.noreply.github.com>
2024-03-10 22:31:20 +0300

    Create tasks.h

---
+++ include/tasks.h
@@ -0,0 +1,14 @@
+// Copyright 2024 Ekaterina Kistrimova
+#ifndef ST_2_TASKS_H
+#define ST_2_TASKS_H
+
+double solvePlanetAndRopeTask(double planetRadius, double radiusIncrement);
+
+double solveSwimmingPoolTask(double poolRadius,
+                             double pathWidth,
+                             double costOfCoverage,
+                             double fencePrice);
+
+#endif //ST_2_TASKS_H
+
+
